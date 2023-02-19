c4680bf4b4e0bfe1a060de00946af1e4d69bbd63
Daniel Macharia <danielhuios@gmail.com>
2023-02-19 03:17:03 +0300

    :art: modifies tasks service to use observables from rxjs

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { TASKS } from '../mock-tasks';
 import { Task } from '../Task'
 
@@ -10,7 +11,9 @@ export class TaskService {
   constructor() { }
 
   //function to get tasks from the component
-  getTasks(): Task[] {
-    return TASKS
+  //add the observable as a type
+  getTasks(): Observable<Task[]> {
+    const tasks = of(TASKS)
+    return tasks
   }
 }
